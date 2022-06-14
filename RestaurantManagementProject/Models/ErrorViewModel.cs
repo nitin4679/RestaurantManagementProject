3b0ad569de4fa1f29161f37cf32d6cdd64a132a8
nitin4679 <nitinjakhar1527@gmail.com>
2022-06-14 14:39:43 -0400

    Add project files.

---
+++ RestaurantManagementProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantManagementProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
